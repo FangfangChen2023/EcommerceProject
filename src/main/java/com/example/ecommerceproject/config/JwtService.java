5fedf0bc2b479c5fddf0dde003527dd118abcfc5
FangfangChen2023 <chinnhouhou@gmail.com>
2023-11-07 16:40:14 +0000

    Adding a Jwt filter

---
+++ src/main/java/com/example/ecommerceproject/config/JwtService.java
@@ -0,0 +1,25 @@
+package com.example.ecommerceproject.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "";
+
+    public String extractUserEmail(String token){
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    private Key getSignInKey(){
+        return null;
+    }
+}
---
+++ src/main/java/com/example/ecommerceproject/domain/Role.java
@@ -0,0 +1,7 @@
+package com.example.ecommerceproject.domain;
+
+public enum Role {
+    CUSTOMER,
+    ADMIN,
+    DELIVERY
+}
