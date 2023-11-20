c0665ccf17446559579f794408cb01913fcb79a2
FangfangChen2023 <chinnhouhou@gmail.com>
2023-11-20 12:07:04 +0000

    Revert "Fix secret key problem"
    This reverts commit 485a13619491112af6c56bfc61938328e380dd7f.

--- src/main/java/com/example/ecommerceproject/config/JwtService.java
+++ src/main/java/com/example/ecommerceproject/config/JwtService.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJJc3N1ZXIgKGlzcykiOiJJc3N1ZXIiLCJJc3N1ZWQgQXQgKGlhdCkiOiIyMDIzLTExLTE5VDEyOjU1OjI0LjkyNloiLCJFeHBpcmF0aW9uIFRpbWUgKGV4cCkiOiIyMDIzLTExLTE5VDEzOjU1OjI0LjkyNloiLCJTdWJqZWN0IChzdWIpIjoiU3ViamVjdCIsIlVzZXJuYW1lIChhdWQpIjoiSmF2YUd1aWRlcyIsIlJvbGUiOiJBRE1JTiJ9LU1tQpohnNa6z1vUlnntETydnl5PdrrH0yvLL9J0";
+    private static final String SECRET_KEY = "skBfq5GBECjWlTSXuCdWJflcogTeF4NM";
 
     public String extractUsername(String token){
         return null;
